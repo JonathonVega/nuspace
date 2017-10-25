8e3a062cd4a94a473bb7d49a9389235f97d4b996
Jonathon Vega <jfvega@live.com>
2017-10-25 18:27:13 -0400

    Added ImageAnnotationView class file

---
+++ nuspace/Model/ImageAnnotationView.swift
@@ -0,0 +1,37 @@
+//
+//  ImageAnnotationView.swift
+//  nuspace
+//
+//  Created by Jonathon F Vega on 10/25/17.
+//  Copyright © 2017 Jonathon Vega. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class ImageAnnotationView: MKAnnotationView {
+    private var imageView: UIImageView!
+    
+    override init(annotation: MKAnnotation?, reuseIdentifier: String?){
+        super.init(annotation: annotation, reuseIdentifier: reuseIdentifier)
+        
+        self.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+        self.imageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        self.addSubview(self.imageView)
+        
+        self.imageView.layer.cornerRadius = 25.0
+        self.imageView.layer.masksToBounds = true
+    }
+    
+    override var image: UIImage? {
+        get {
+            return self.imageView.image
+        }
+        set{
+            self.imageView.image = newValue
+        }
+    }
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
